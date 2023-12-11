565b6e3140af722419d9c36d57592b6f6d331b7c
LeeYuJ1n <152352879+LeeYuJ1n@users.noreply.github.com>
2023-12-11 18:31:08 +0900

    Add files via upload

---
+++ 22(화)/WinAPI(과제포함)/WinAPI/Framework.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN
+
+#define WIN_WIDTH 1280
+#define WIN_HEIGHT 720
+
+#define CENTER_X (WIN_WIDTH * 0.5f)
+#define CENTER_Y (WIN_HEIGHT * 0.5f)
+
+#define DELTA Timer::Get()->GetElapsedTime()
+
+#define KEY_DOWN(k) Keyboard::Get()->Down(k)
+#define KEY_UP(k) Keyboard::Get()->Up(k)
+#define KEY_PRESS(k) Keyboard::Get()->Press(k)
+
+#define PHI 3.1415926f
+
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <math.h>
+
+using namespace std;
+
+//Framework Header
+#include "Framework/Utilities/Singleton.h"
+#include "Framework/Utilities/Timer.h"
+#include "Framework/Utilities/Keyboard.h"
+
+#include "Framework/Math/Vector2.h"
+#include "Framework/Math/GameMath.h"
+
+using namespace GameMath;
+
+//Object Header
+#include "Objects/Basic/GameObject.h"
+#include "Objects/Basic/Rect.h"
+#include "Objects/Basic/Circle.h"
+
+#include "Objects/Spawn/SpawnObject.h"
+#include "Objects/Spawn/SpawnManager.h"
+
+//Scene Header
+#include "Scenes/Scene.h"
+
+//Manager Header
+#include "Manager/GameManager.h"
+
+extern Vector2 mousePos;
+extern HWND _hWnd;
+extern HINSTANCE hInst;
