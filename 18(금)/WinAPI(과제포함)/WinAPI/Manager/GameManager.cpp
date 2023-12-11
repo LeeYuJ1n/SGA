6e90a0e7300995139520ec892fc157b0da4c1751
LeeYuJ1n <152352879+LeeYuJ1n@users.noreply.github.com>
2023-12-11 18:29:10 +0900

    Add files via upload

---
+++ 18(금)/WinAPI(과제포함)/WinAPI/Manager/GameManager.cpp
@@ -0,0 +1,58 @@
+#include "Framework.h"
+
+#include "Scenes/TutorialScene.h"
+#include "Scenes/SpawnBrute.h"
+#include "Scenes/SpawnUsingManager.h"
+#include "Scenes/SpawnCircleScene.h"
+
+GameManager* GameManager::instance = nullptr;
+
+GameManager::GameManager()    
+{    
+    // scene = new TutorialScene();
+    // scene = new SpawnBrute();
+    // scene = new SpawnUsingManager();
+    scene = new SpawnCircleScene();
+}
+
+GameManager::~GameManager()
+{
+    delete scene;
+
+    Timer::Delete();
+    Keyboard::Delete();
+}
+
+void GameManager::Init(HWND hWnd)
+{
+    this->hWnd = hWnd;
+    hdc = GetDC(hWnd);
+
+    hBackDC = CreateCompatibleDC(hdc);
+    hBackBitmap = CreateCompatibleBitmap(hdc, WIN_WIDTH, WIN_HEIGHT);
+    SelectObject(hBackDC, hBackBitmap);    
+
+    Timer::Get();
+    Keyboard::Get();
+}
+
+void GameManager::Update()
+{
+    Timer::Get()->Update();
+    Keyboard::Get()->Update();
+
+    scene->Update();
+}
+
+void GameManager::Render()
+{
+    PatBlt(hBackDC, 0, 0, WIN_WIDTH, WIN_HEIGHT, WHITENESS);
+
+    //InvalidateRect(hWnd, nullptr, false);
+    scene->Render(hBackDC);
+
+    Timer::Get()->Render(hBackDC);
+
+    BitBlt(hdc, 0, 0, WIN_WIDTH, WIN_HEIGHT,
+        hBackDC, 0, 0, SRCCOPY);
+}
