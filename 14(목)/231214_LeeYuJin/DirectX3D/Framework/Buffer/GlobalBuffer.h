733fd7a2a2bdae7421463ddf03a2ca69eb25cfd3
LeeYuJ1n <152352879+LeeYuJ1n@users.noreply.github.com>
2023-12-15 09:19:56 +0900

    [ Direct X 3D ] map 적용해보기, 셰이더 호출 복습

---
+++ 14(목)/231214_LeeYuJin/DirectX3D/Framework/Buffer/GlobalBuffer.h
@@ -0,0 +1,107 @@
+#pragma once
+
+class MatrixBuffer : public ConstBuffer
+{
+public:
+    MatrixBuffer() : ConstBuffer(&matrix, sizeof(Matrix))
+    {
+        matrix = XMMatrixIdentity();
+    }
+
+    void Set(Matrix value)
+    {
+        matrix = XMMatrixTranspose(value);
+    }
+
+private:
+    Matrix matrix;
+};
+
+class ViewBuffer : public ConstBuffer
+{
+private:
+    struct Data
+    {
+        Matrix view;
+        Matrix invView;
+    };
+
+public:
+    ViewBuffer() : ConstBuffer(&data, sizeof(Data))
+    {
+        data.view = XMMatrixIdentity();
+        data.invView = XMMatrixIdentity();
+    }
+
+    void Set(Matrix view, Matrix invView)
+    {
+        data.view = XMMatrixTranspose(view);
+        data.invView = XMMatrixTranspose(invView);
+    }
+
+private:
+    Data data;
+};
+
+class ColorBuffer : public ConstBuffer
+{
+public:
+    ColorBuffer() : ConstBuffer(&color, sizeof(Float4))
+    {
+    }
+
+    Float4& Get() { return color; }
+
+private:
+    Float4 color = { 1, 1, 1, 1 };
+};
+
+class IntValueBuffer : public ConstBuffer
+{
+public:
+    IntValueBuffer() : ConstBuffer(values, sizeof(int) * 4)
+    {
+    }
+
+    int* Get() { return values; }
+
+private:
+    int values[4] = {};
+};
+
+class FloatValueBuffer : public ConstBuffer
+{
+public:
+    FloatValueBuffer() : ConstBuffer(values, sizeof(float) * 4)
+    {
+    }
+
+    float* Get() { return values; }
+
+private:
+    float values[4] = {};
+};
+
+class LightBuffer : public ConstBuffer
+{
+private:
+    struct Light
+    {
+        Float4 color = { 1, 1, 1, 1 };
+        Float3 direction = { 0, -1, 1 };
+
+        float shininess = 24.0f;
+
+        Float4 ambient = { 0.1f, 0.1f, 0.1f, 1.0f };
+    };
+
+public:
+    LightBuffer() : ConstBuffer(&light, sizeof(Light))
+    {
+    }
+
+    Light& Get() { return light; }
+
+private:
+    Light light;
+};
---
+++ 14(목)/231214_LeeYuJin/DirectX3D/Framework/Math/GameMath.h
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace GameMath
+{
+    enum class Direction
+    {
+        UP, DOWN, LEFT, RIGHT, NONE
+    };
+
+    int Random(const int& min, const int& max);
+    float Random(const float& min, const float& max);
+    Vector2 Random(const Vector2& min, const Vector2& max);
+
+    float Clamp(const float& min, const float& max, float value);
+
+    float Lerp(const float& start, const float& end, float t);
+    Vector2 Lerp(const Vector2& start, const Vector2& end, float t);    
+    Vector2 SLerp(const Vector2& start, const Vector2& end, float t);    
+    
+    //float Cross(const Vector2& vec1, const Vector2& vec2);
+    //float Dot(const Vector2& vec1, const Vector2& vec2);
+
+    Vector3 Cross(Vector3& vec1, Vector3& vec2);
+    float Dot(Vector3& vec1, Vector3& vec2);
+
+    float Distance(const Vector2& vec1, const Vector2& vec2);
+
+    bool NearlyEqual(const float& s1, const float& s2);
+}
