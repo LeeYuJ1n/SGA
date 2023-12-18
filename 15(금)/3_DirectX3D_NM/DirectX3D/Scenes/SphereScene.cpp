a96446618a6da922aae76600b12e5eedb018f33f
LeeYuJ1n <152352879+LeeYuJ1n@users.noreply.github.com>
2023-12-18 15:14:35 +0900

    [ DirectX3D ] 광선을 이용한 박스 충돌

---
+++ 15(금)/3_DirectX3D_NM/DirectX3D/Scenes/SphereScene.cpp
@@ -0,0 +1,60 @@
+#include "Framework.h"
+#include "SphereScene.h"
+
+SphereScene::SphereScene()
+{
+    // 태양
+    sphere = new Sphere();    
+    sphere->GetMaterial()->SetShader(L"Basic/Texture.hlsl");
+    sphere->GetMaterial()->SetDiffuseMap(L"Textures/Etc/Sun.jpg");
+
+    // 지구
+    earth = new Sphere();
+    earth->GetMaterial()->SetDiffuseMap(L"Textures/Etc/Earth_DM.jpg");
+
+    earth->SetParent(sphere);
+    earth->Pos().x = 10.0f;
+
+    // 고양이 스피어
+    testSphere = new Sphere();
+    testSphere->GetMaterial()->SetShader(L"Basic/Texture.hlsl");
+    testSphere->GetMaterial()->SetDiffuseMap(L"Textures/Etc/cat.jpg");
+    testSphere->Pos().y = 5.0f;
+}
+
+SphereScene::~SphereScene()
+{
+    delete sphere;
+    delete earth;
+    delete testSphere;
+}
+
+void SphereScene::Update()
+{
+    sphere->Rot().y += DELTA;
+    sphere->UpdateWorld();
+
+    earth->UpdateWorld();
+    
+    testSphere->UpdateWorld();
+}
+
+void SphereScene::PreRender()
+{
+}
+
+void SphereScene::Render()
+{
+    sphere->Render();
+    earth->Render();
+    testSphere->Render();
+}
+
+void SphereScene::PostRender()
+{
+}
+
+void SphereScene::GUIRender()
+{
+    sphere->RenderUI();
+}
