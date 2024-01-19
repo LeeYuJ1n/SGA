bab794df8f5ca6b80b7831a3b99df8115e0ab061
LeeYuJ1n <kikkil2594@gmail.com>
2024-01-19 18:58:29 +0900

    [ DirectX3D ] NULL

---
+++ 22(월)/DirectX3D_RenderTarget/DirectX3D/Framework/Render/GBuffer.cpp
@@ -0,0 +1,21 @@
+#include "Framework.h"
+
+GBuffer::GBuffer()
+{
+}
+
+GBuffer::~GBuffer()
+{
+}
+
+void GBuffer::SetMultiRenderTarget()
+{
+}
+
+void GBuffer::PostRender()
+{
+}
+
+void GBuffer::SetSRVs()
+{
+}
