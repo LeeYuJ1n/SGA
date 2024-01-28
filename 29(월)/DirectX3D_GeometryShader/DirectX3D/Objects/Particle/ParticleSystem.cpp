b98ed8993601c59ce9c6e3225d1192b07bc7d012
LeeYuJ1n <kikkil2594@gmail.com>
2024-01-28 23:10:35 +0900

    NULL

---
+++ 29(월)/DirectX3D_GeometryShader/DirectX3D/Objects/Particle/ParticleSystem.cpp
@@ -0,0 +1,50 @@
+#include "Framework.h"
+#include "ParticleSystem.h"
+
+ParticleSystem::ParticleSystem(string file)
+{
+}
+
+ParticleSystem::ParticleSystem(wstring file)
+{
+}
+
+ParticleSystem::~ParticleSystem()
+{
+}
+
+void ParticleSystem::Update()
+{
+}
+
+void ParticleSystem::Render()
+{
+}
+
+void ParticleSystem::Play(Vector3 pos, Vector3 rot)
+{
+}
+
+void ParticleSystem::Stop()
+{
+}
+
+void ParticleSystem::UpdatePhysical()
+{
+}
+
+void ParticleSystem::UpdateColod()
+{
+}
+
+void ParticleSystem::Init()
+{
+}
+
+void ParticleSystem::LoadData(string file)
+{
+}
+
+void ParticleSystem::LoadData(wstring file)
+{
+}
